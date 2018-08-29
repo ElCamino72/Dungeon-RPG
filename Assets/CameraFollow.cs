5d10e8713883f1bace1eb85a6e8a6a4fe712c0d5
ElCamino72 <anthonyb@mobileintegration-group.com>
2018-08-29 10:01:57 -0700

    Follow Cam
    Create camera arm to follow player

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	private Transform playerTransform;
+
+	// Use this for initialization
+	void Start () {
+		// Grab Player Transform
+		playerTransform = GameObject.FindWithTag ("Player").GetComponent <Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void LateUpdate () {
+		Vector3 playerPosition = playerTransform.position;
+		transform.SetPositionAndRotation (playerPosition, transform.rotation);
+	}
+}
